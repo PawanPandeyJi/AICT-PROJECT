47f702cc0377d23209c536d39061424e80c958a4
PawanPandeyJi <pawankrpandeyjii@gmail.com>
2025-05-04 18:06:30 +0530

    Add env file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import healthRouter from './routers/health.router.js'
 
@@ -6,7 +8,7 @@ const app = express()
 app.use(healthRouter)
 
 
-const PORT = 8000
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Your server is runnig on port ${PORT}`)
